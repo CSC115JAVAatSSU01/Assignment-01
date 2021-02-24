72d8935f8d19c38fcc36156d452f47fa1b8e0612
fnafaatSSU <72876690+fnafaatSSU@users.noreply.github.com>
2021-02-24 13:07:33 -0500

    Add files via upload

---
+++ Assignment#01.java
@@ -0,0 +1,16 @@
+class Assignment#01
+{ 
+  /* CSC 115: Assignment#01
+
+Deadline: 02/ 28/ 2021. 11:59 PM
+
+8.15 (Rational Numbers) Create a class called Rational for performing arithmetic with fractions. Write a program to test your class. Use integer variables to represent the private instance variables of the class—the numerator and the denominator. Provide a constructor that enables an object of this class to be initialized when it’s declared. The constructor should store the fraction in reduced form. The fraction 2/4 is equivalent to 1/2 and would be stored in the object as 1 in the numerator and 2 in the denominator. Provide a no-argument constructor with default values in case no initializers are provided. Provide public methods that perform each of the following operations:
+a) Add two Rational numbers: The result of the addition should be stored in reduced form. Implement this as a static method.
+b) Subtract two Rational numbers: The result of the subtraction should be stored in reduced form. Implement this as a static method.
+c) Multiply two Rational numbers: The result of the multiplication should be stored in reduced form. Implement this as a static method.
+d) Divide two Rational numbers: The result of the division should be stored in reduced form. Implement this as a static method.
+e) Return a String representation of a Rational number in the form a/b, where a is the numerator and b is the denominator.
+f) Return a String representation of a Rational number in floating-point format. (Consider providing formatting capabilities that enable the user of the class to specify the number of digits of precision to the right of the decimal point.)
+8.16 (Huge Integer Class) Create a class HugeInteger which uses a 40-element array of digits to store integers as large as 40 digits each. Provide methods parse, toString, add and subtract. Method parse should receive a String, extract each digit using method charAt and place the integer equivalent of each digit into the integer array. For comparing HugeInteger objects, provide the following methods: isEqualTo, isNotEqualTo, isGreaterThan, isLessThan, isGreaterThanOrEqualTo and isLessThanOrEqualTo. Each of these is a predicate method that returns true if the relationship holds between the two HugeInteger objects and returns false if the relationship does not hold. Provide a predicate method isZero. If you feel ambitious, also provide methods multiply, divide and remainder. [Note: Primitive boolean values can be output as the word “true” or the word “false” with format specifier %b.]
+*/
+} 
